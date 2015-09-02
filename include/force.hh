57262a0ce1981913713d5139a7db80d617494ff6
Matthew Grasinger <grasingerm@gmail.com>
2015-09-02 12:30:03 -0400

    External forcing
    Add: base class for external forces
    Add: public interface for external forces
    Add: implementation for sukop forcing
    Add: implementation for guo forcing

---
+++ include/force.hh
@@ -0,0 +1,92 @@
+#ifndef FORCE_HH
+#define FORCE_HH 
+
+// Complex flow simulator using lattice Boltzmann method
+// Copyright (C) 2015 Matthew Grasinger
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// A copy of the GNU General Public License is at the root directory of
+// this program.  If not, see <http://www.gnu.org/licenses/>
+
+#include <armadillo>
+#include "balbm_config.hh"
+#include "lattice.hh"
+
+namespace balbm
+{
+
+namespace d2q9
+{
+
+//! \class AbstractForce
+//!
+//! \brief Abstract base class for force implementations
+//!
+//! Provide polymorphic behavior for forces and force implementations
+class AbstractForce
+{
+public:
+  virtual ~AbstractForce()=0;
+  AbstractForce(const std::array<double, 2>& F) : F_(F) {}
+  inline arma::vec::fixed<2> u_trans(const Lattice& lat, const arma::vec& u) 
+    const { return u_trans_(lat, u); }
+  inline double f_col(const Lattice& lat, const double omega, 
+                      const arma::vec& u, const unsigned k) const
+    { return f_col_(lat, omega, u, k); }
+  inline const std::array<double, 2>& F() const { return F_; }
+private:
+  std::array<double, 2> F_;
+  virtual arma::vec::fixed<2> u_trans_(const Lattice&, const arma::vec&) 
+    const=0; 
+  virtual double f_col_(const Lattice&, const double omega, const arma::vec&, 
+                        const unsigned k) const=0;
+};
+
+//! \class SukopThorneForce
+//!
+//! \brief Force implementation from Sukop and Thorne 2005
+//!
+//! One of the forcing implementations found in Lattice Boltzmann Method for
+//! Geoscientists and Engineers by Sukop and Thorne 2005
+class SukopThorneForce : public AbstractForce
+{
+public:
+  ~SukopThorneForce() {};
+  SukopThorneForce(const std::array<double, 2>& F) : AbstractForce(F) {}
+private:
+  arma::vec::fixed<2> u_trans_(const Lattice&, const arma::vec&) const; 
+  double f_col_(const Lattice&, const double, const arma::vec&, 
+                const unsigned) const;
+};
+
+//! \class GuoForce
+//!
+//! \brief Force implementation from Guo 2002
+//!
+//! One of the forcing implementations found in Discrete lattice effects on the
+//! forcing termin the lattice Boltzmann method Guo et. al. 2002
+class GuoForce : public AbstractForce
+{
+public:
+  ~GuoForce() {};
+  GuoForce(const std::array<double, 2>& F) : AbstractForce(F) {}
+private:
+  arma::vec::fixed<2> u_trans_(const Lattice&, const arma::vec&) const; 
+  double f_col_(const Lattice&, const double, const arma::vec&, 
+                const unsigned) const;
+};
+
+} // namespace d2q9
+
+} // namespace balbm
+
+#endif // FORCE_HH
---
+++ src/force.cc
@@ -0,0 +1,82 @@
+// Complex flow simulator using lattice Boltzmann method
+// Copyright (C) 2015 Matthew Grasinger
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// A copy of the GNU General Public License is at the root directory of
+// this program.  If not, see <http://www.gnu.org/licenses/>
+
+#include "force.hh"
+#include <armadillo>
+
+namespace balbm
+{
+
+namespace d2q9
+{
+
+//! Virtual destructor for AbstractForce base class
+~AbstractForce() {}
+
+//! Transforms macroscopic velocity vector to simulate external forces
+//!
+//! \param lat Lattice
+//! \param u Macroscopic velocity vector
+//! \return Transformed velocity vector
+arma::vec::fixed<2> SukopThorneForce::u_trans
+  (const Lattice& lat, const arma::vec& u) const
+{
+  return u;
+}
+
+//! Value to add to particle distributions to simulate effect of external forces
+//!
+//! \param lat Lattice
+//! \param omega Collision frequency
+//! \param u Macroscopic velocity vector
+//! \param k Index of lattice direction
+double SukopThorneForce::f_col_(const Lattice& lat, const double omega, 
+                                const arma::vec& u, const unsigned k)
+{
+  const arma::vec::fixed<2> ck(lat.cp(k), 2, false, true);
+  return lat.w(k) * lat.dt() / lat.cssq() * arma::dot(F(), ck);
+}
+
+//! Transforms macroscopic velocity vector to simulate external forces
+//!
+//! \param lat Lattice
+//! \param u Macroscopic velocity vector
+//! \return Transformed velocity vector
+arma::vec::fixed<2> GuoForce::u_trans
+  (const Lattice& lat, const arma::vec& u) const
+{
+  return u + (lat.dt() / 2.0 * F());
+}
+
+//! Value to add to particle distributions to simulate effect of external forces
+//!
+//! \param lat Lattice
+//! \param omega Collision frequency
+//! \param u Macroscopic velocity vector
+//! \param k Index of lattice direction
+double GuoForce::f_col_(const Lattice& lat, const double omega, 
+                        const arma::vec& u, const unsigned k)
+{
+  const arma::vec::fixed<2> ck(lat.cp(k), 2, false, true);
+  return ((1 - 0.5 * omega) * lat.w(k) 
+           * arma::dot((ck - u)/(lat.cssq()) 
+                        - arma::dot(ck, u) / (lat.cssq()*lat.cssq()) * ck,
+                       F()));
+}
+
+} // namespace d2q9
+
+} // namespace balbm
